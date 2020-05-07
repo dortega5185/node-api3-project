9f47bc6f111813a3a7a069a78c1d4cbb23768e4f
David Mizativan-Ortega <dortega5185@gmail.com>
2020-05-07 11:27:34 -0500

    to deploy

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const server = require("./server.js");
 const colors = require("colors");
 
-server.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
   console.log(
-    "\n* Server Running on http://localhost:5000 *\n".bgWhite.underline.black
+    `\n* Server Running on http://localhost:${port} *\n`.bgWhite.underline.black
   );
 });
