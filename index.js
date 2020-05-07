0c750a60a52949210d181e8965aee0f1d75caac3
David Mizativan-Ortega <dortega5185@gmail.com>
2020-05-07 03:26:27 -0500

    finished mvp

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const server = require("./server.js");
+const colors = require("colors");
+
+server.listen(5000, () => {
+  console.log(
+    "\n* Server Running on http://localhost:5000 *\n".bgWhite.underline.black
+  );
+});
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-const express = require('express');
+const express = require("express");
+
+const postRouter = require("./posts/postRouter");
+
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.use(logger);
+
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const today = new Date().toISOString();
+  console.log(`[${today}] ${req.method} to ${req.url}`);
+
+  next();
+}
 
 module.exports = server;
